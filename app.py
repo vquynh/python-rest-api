d1bb5fb09f149640b831211855719658d36ffaf4
Quynh <quynh.nguyen@deposit-solutions.com>
2017-10-15 15:46:26 +0200

    first commit, added rest api

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_jwt import JWT
+from flask_restful import Api
+from resources.item import ItemList, Item
+
+from resources.user import UserRegister
+from resources.store import Store, StoreList
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'quynh'
+api = Api(app)
+
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(ItemList, '/items')
+
+api.add_resource(Item, '/item/<string:name>')
+
+api.add_resource(StoreList, '/stores')
+
+api.add_resource(Store, '/store/<string:name>')
+
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
